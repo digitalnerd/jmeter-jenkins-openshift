898ae1d76bc970bdd0143497bbef28fc9238b7a2
Digital Nerd <33043683+digitalnerd@users.noreply.github.com>
2019-12-12 08:01:54 +0300

    fixed

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-for i in $(ls -1 Tests | grep jmx)
-do
-    echo "*** Testing a ${i} file"
-    jmeter -n -t Tests/${i} -l Results/${i}-results.jtl
-    cat Results/${i}-results.jtl
-done
+jmeter -n -t Tests/$2.jmx -l Results/$2-results.jtl
+
+cat Results/$2-results.jtl
+
+curl -X POST -d "$HOSTNAME" $1
 
 sleep 10
